e121e05daa5d49081d831802d56f47db8c3dc7ba
Oleg Bulatov <obulatov@redhat.com>
2017-06-16 16:56:40 +0200

    Update gettimeofday
    Signed-off-by: Oleg Bulatov <obulatov@redhat.com>

--- opt/gettimeofday/main.c
+++ opt/gettimeofday/main.c
@@ -8,19 +8,5 @@ int main() {
         perror("gettimeofday");
         exit(EXIT_FAILURE);
     }
-    printf("%lld%06d\n", (long long)(tv.tv_sec), tv.tv_usec);
+    printf("%lld%06ld\n", (long long)(tv.tv_sec), (long)(tv.tv_usec));
 }
-
-/*
-#define _GNU_SOURCE
-#include <unistd.h>
-#include <sys/syscall.h>
-#include <sys/types.h>
-
-int main() {
-        syscall(SYS_write, 1, "Hello, worldn", 13);
-
-        pid_t tid = syscall(SYS_gettid);
-        tid = syscall(SYS_tgkill, getpid(), tid);
-}
-*/
